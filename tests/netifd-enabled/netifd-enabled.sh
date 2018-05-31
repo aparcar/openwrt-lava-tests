bab1279c6afd1ebfe57f124e8bbe0c15dfb55717
Alexander Couzens <lynxis@fe80.eu>
2018-06-01 01:44:41 +0200

    netifd-enabled: do network.lan.enabled=1 ; =0 ; =1 and check routes present

---
+++ tests/netifd-enabled/netifd-enabled.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+# this should run on the devicea
+
+_log() {
+	echo "$@"
+}
+
+check_rules() {
+	case=$1
+	rule=$2
+
+	if ! ip rule show |grep "$rule" ; then
+		_log "$case Can not find rule $rule"
+		lava-test-case netifd-enabled --result failure
+		exit 1
+	fi
+}
+
+LANIP=192.168.91.1
+LANNET=192.168.91.0
+
+_log "disabled kernel messages"
+dmesg -n 1
+
+uci show
+uci set network.lan.enabled=1
+uci set network.lan.ipaddr=$LANIP
+ubus call network reload
+
+# check if lan address is assigned
+if ! ip -o a show | grep "inet $LANIP" ; then
+    _log "$LANIP is not bind to any interface"
+	lava-test-case netifd-enabled --result failure
+    exit 1
+fi
+
+if ! ip route show | grep "$LANNET" ; then
+    _log "$LANNET is not in the routing table"
+	lava-test-case netifd-enabled --result failure
+    exit 1
+fi
+
+# disable the network
+uci set network.lan.enabled=0
+ubus call network reload
+
+sleep 3
+
+# enable again
+uci set network.lan.enabled=1
+ubus call network reload
+
+# check if lan address is assigned
+if ! ip -o a show | grep "inet $LANIP" ; then
+    _log "$LANIP Test: is not bind to any interface"
+	lava-test-case netifd-enabled --result failure
+    exit 1
+fi
+
+if ! ip route show | grep "$LANNET" ; then
+    _log "$LANNET Test: is not in the routing table"
+	lava-test-case netifd-enabled --result failure
+    exit 1
+fi
+
+_log "adding rules"
+uci set network.guestable=rule
+uci set network.guestable.in=lan
+uci set network.guestable.lookup='1001'
+uci set network.guestable.priority='100'
+uci set network.unreach=rule
+uci set network.unreach.in=lan
+uci set network.unreach.action='unreachable'
+uci set network.unreach.priority='110'
+ubus call network reload
+sleep 5
+
+_log "check if rules appear"
+ip rule show
+check_rules rules "^100:"
+check_rules rules "^110:"
+
+_log "reloading netifd"
+uci set network.lan.enabled=0
+ubus call network reload
+sleep 5
+uci set network.lan.enabled=1
+ubus call network reload
+sleep 5
+
+_log "check if rules appear after reloading with enabled"
+ip rule show
+check_rules rules "^100:"
+check_rules rules "^110:"
+
+lava-test-case netifd-enabled --result pass
