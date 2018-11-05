3ba691390ed32bd2e56dc91490e841860a2fcb5d
Alexander Couzens <lynxis@fe80.eu>
2018-11-05 08:23:09 +0100

    tests/basic: use lava-test-case directly from the basic.yml
    Remove measurement, as measurement requires numeris

--- tests/basic/basic.sh
+++ tests/basic/basic.sh
@@ -8,8 +8,5 @@ _log() {
 _log "disabled kernel messages"
 dmesg -n 1
 
-_log "cat /etc/openwrt_release"
-cat /etc/openwrt_release
-lava-test-case openwrt-version --result pass --measurement "$(cat /etc/openwrt_version)"
 _log "list opkg installed packages"
 opkg list_installed
