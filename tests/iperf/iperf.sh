9dd983b821d2068f21784e1c14c6f9ff71aaf0c7
Alexander Couzens <lynxis@fe80.eu>
2018-11-06 19:02:58 +0100

    tests/iperf/iperf.sh: remove parsing the result of iperf
    this is a leftover from an early test setup

--- tests/iperf/iperf.sh
+++ tests/iperf/iperf.sh
@@ -11,9 +11,4 @@ iperf -c iperf.lava -f m >/tmp/iperf.output 2>&1
 cat /tmp/iperf.output
 MBIT=$(tail -n1 /tmp/iperf.output | awk '{ print $7 }')
 
-if echo "$MBIT" | cut -c1 | grep -q 0 ; then
-    lava-test-case iperf-result --result fail --measurement "$MBIT" --units Mbits/sec
-else
-    lava-test-case iperf-result --result pass --measurement "$MBIT" --units Mbits/sec
-fi
-
+lava-test-case iperf-result --result pass --measurement "$MBIT" --units Mbits/sec
